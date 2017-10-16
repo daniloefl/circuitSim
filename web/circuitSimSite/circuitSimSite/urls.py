8d758e8f295322aed11110094f5d33b02367583c
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-10-17 00:21:42 +0200

    Removed admin.

--- web/circuitSimSite/circuitSimSite/urls.py
+++ web/circuitSimSite/circuitSimSite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('circuitSim.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+#    url(r'^admin/', include(admin.site.urls)),
 ]
