9215dbb2096c1bd167726830dd704e0af7e8e74d
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-10-17 00:21:19 +0200

    Removed admin and debug mode.

--- web/circuitSimSite/circuitSimSite/settings.py
+++ web/circuitSimSite/circuitSimSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'igcx0sx3iu$6%-y78()gr%rhy@r#rv5no3y-aq#8mok825n6x$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
